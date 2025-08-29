d163299948d337182da30d3779dc91156bd90b67
MayurRV <MayurRV2002@gmail.com>
2025-08-29 16:02:53 +0530

    Add to cart been solved

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,50 +1,130 @@
-import React, { createContext, useState } from 'react'
+// import React, { createContext, useState } from 'react'
+// import all_product from '../components/assets/all_product'
+
+// export let ShopContext=createContext(null);
+
+// let getDefaultCart=()=>{
+//   let cart={};
+//   for(let index=0 ; index<all_product.length ; index++){
+//     cart[index]=0
+//   }
+//   return cart;
+// }
+
+// const ShopContextProvider = ({children}) => {
+
+//   let [cartItems,setCartItems]=useState(getDefaultCart())
+
+//   let addToCart=(itemId)=>{
+//     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+//     // console.log(addToCart);
+//   }
+
+//   let removeToCart=(itemId)=>{
+//     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+//   }
+
+//   let emptyresetcart=(itemId)=>{
+//     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]*0}))
+//   }
+
+//   let getToCartAmount=()=>{
+
+//     let totalAmount =0;
+//     for(const item in cartItems){
+//       if(cartItems[item]>0){
+//         let itemInfo=all_product.find((product)=>product.id === Number(item));
+//         totalAmount+=cartItems[item]*itemInfo.new_price;
+//       }
+//     }
+//     return totalAmount;
+//   };
+
+//   let getTotalCartCount=()=>{
+//     let totalItem=0;
+//     for( const item in cartItems){
+//       if(cartItems[item]>0){
+//         totalItem+=cartItems[item];
+//       }
+//     }
+//     return totalItem;
+//   };
+
+//   console.log(cartItems);
+
+//   let contextValue={all_product,cartItems,addToCart,removeToCart,getToCartAmount,getTotalCartCount,emptyresetcart}
+  
+//   return (
+//     <ShopContext.Provider value={contextValue}>
+//       {children}
+//     </ShopContext.Provider>
+//   )
+// }
+
+// export default ShopContextProvider
+
+//-------------Gauravs code---------------
+
+
+import React, { createContext, useEffect, useState } from 'react'
 import all_product from '../components/assets/all_product'
 
-export let ShopContext=createContext(null);
+export let ShopContext = createContext(null);
 
-let getDefaultCart=()=>{
-  let cart={};
-  for(let index=0 ; index<all_product.length ; index++){
-    cart[index]=0
+let getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length; index++) {
+    cart[index] = 0
   }
   return cart;
 }
 
-const ShopContextProvider = ({children}) => {
+const ShopContextProvider = ({ children }) => {
 
-  let [cartItems,setCartItems]=useState(getDefaultCart())
+  let [cartItems, setCartItems] = useState(() => {
 
-  let addToCart=(itemId)=>{
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+    const cartData = sessionStorage.getItem("cart")
+    return cartData ? JSON.parse(cartData) : getDefaultCart();
+
+
+  })
+
+  useEffect(() => {
+
+    sessionStorage.setItem("cart", JSON.stringify(cartItems))
+  }, [cartItems])
+
+
+  let addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     // console.log(addToCart);
   }
 
-  let removeToCart=(itemId)=>{
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  let removeToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
   }
 
-  let emptyresetcart=(itemId)=>{
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]*0}))
+  let emptyresetcart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] * 0 }))
   }
 
-  let getToCartAmount=()=>{
+  let getToCartAmount = () => {
 
-    let totalAmount =0;
-    for(const item in cartItems){
-      if(cartItems[item]>0){
-        let itemInfo=all_product.find((product)=>product.id === Number(item));
-        totalAmount+=cartItems[item]*itemInfo.new_price;
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.new_price;
       }
     }
     return totalAmount;
   };
 
-  let getTotalCartCount=()=>{
-    let totalItem=0;
-    for( const item in cartItems){
-      if(cartItems[item]>0){
-        totalItem+=cartItems[item];
+  let getTotalCartCount = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
       }
     }
     return totalItem;
@@ -52,8 +132,8 @@ const ShopContextProvider = ({children}) => {
 
   console.log(cartItems);
 
-  let contextValue={all_product,cartItems,addToCart,removeToCart,getToCartAmount,getTotalCartCount,emptyresetcart}
-  
+  let contextValue = { all_product, cartItems, addToCart, removeToCart, getToCartAmount, getTotalCartCount, emptyresetcart }
+
   return (
     <ShopContext.Provider value={contextValue}>
       {children}
--- src/components/RelatedItems/RelatedItems.jsx
+++ src/components/RelatedItems/RelatedItems.jsx
@@ -1,3 +1,45 @@
+// import React from 'react'
+// import data_product from '../assets/data'
+// import './RelatedItems.css'
+// import Item from '../Item/Item'
+// import all_product from '../assets/all_product'
+
+// const RelatedItems = (props) => {
+  
+//   let {display}=props;
+
+//   return (
+//     <div className='relatedProducts'>
+//         <h1>Related Products</h1>
+//         <hr />
+//         <div className="relatedProducts-items">
+//             {/* {data_product.map((item,i)=>{
+//                 return(
+//                     <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+//                 )
+//             })} */}
+
+//             {all_product.filter(item=>item.category===display.category).slice(4,8).map((item,i)=>{
+
+//               // if(display.category == item.category){
+//                 return(
+//                     <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} emptyresetcart={0}/>
+//                 )
+//               // }
+              
+                
+//             })}
+
+//         </div>
+      
+//     </div>
+//   )
+// }
+
+// export default RelatedItems
+
+//----------Gauravs code edit
+
 import React from 'react'
 import data_product from '../assets/data'
 import './RelatedItems.css'
@@ -5,33 +47,33 @@ import Item from '../Item/Item'
 import all_product from '../assets/all_product'
 
 const RelatedItems = (props) => {
-  
-  let {display}=props;
+
+  let { display } = props;
 
   return (
-    <div className='relatedProducts'>
-        <h1>Related Products</h1>
-        <hr />
-        <div className="relatedProducts-items">
-            {/* {data_product.map((item,i)=>{
+    <div className='relatedProducts' >
+      <h1>Related Products</h1>
+      <hr />
+      <div className="relatedProducts-items" onClick={()=>{window.scrollTo({top:0,behaviour:"smooth"}); window.location.reload();}}>
+        {/* {data_product.map((item,i)=>{
                 return(
                     <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
                 )
             })} */}
 
-            {all_product.filter(item=>item.category===display.category).slice(4,8).map((item,i)=>{
+        {all_product.filter(item => item.category === display.category).slice(4, 8).map((item, i) => {
 
-              // if(display.category == item.category){
-                return(
-                    <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} emptyresetcart={0}/>
-                )
-              // }
-              
-                
-            })}
+          // if(display.category == item.category){
+          return (
+            <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} emptyresetcart={0} />
+          )
+          // }
 
-        </div>
-      
+
+        })}
+
+      </div>
+
     </div>
   )
 }
